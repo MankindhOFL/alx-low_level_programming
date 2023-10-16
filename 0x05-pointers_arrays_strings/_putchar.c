60c426b4563ed457078c283211b8880feafef2af
MankindhOFL <mankindhhawwal@gmail.com>
2023-10-16 10:24:22 -0700

    0-98

---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,16 @@
+0x00-hello_world
+0x01-variables_if_else_while
+#include <unistd.h>
+  
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+return (write(1, &c, 1));
+}
+
